36508fc0de25c64ef9ea959be6d61b8d4ff7aa34
Maheerali-pk <maheerali121@gmail.com>
2023-02-25 22:50:04 +0500

    A wrapper component for all auth pages is created

--- components/AuthPageWrapper.tsx
+++ components/AuthPageWrapper.tsx
@@ -1,9 +1,29 @@
 interface AuthPageWrapperProps {
    icon: JSX.Element;
+   heading: string;
+   subHeading: JSX.Element | string;
+
+   children: React.ReactNode;
 }
 
-const AuthPageWrapper: React.FC<AuthPageWrapperProps> = ({ icon }) => {
-   return <div className="flex flex-col w-full h-full mt-12 my"></div>;
+const AuthPageWrapper: React.FC<AuthPageWrapperProps> = ({
+   icon,
+   heading,
+   subHeading,
+   children,
+}) => {
+   return (
+      <div className="flex justify-center w-full">
+         <div className="flex flex-col items-center w-full md:w-fit h-full pt-12 md:pt-24 px-4 ">
+            <div className="mb-6">{icon}</div>
+            <div className="text-2xl md:text-3xl font-semibold text-gray-900 mb-2 md:mb-3">
+               {heading}
+            </div>
+            <div className="mb-8 text-gray-600">{subHeading}</div>
+            {children}
+         </div>
+      </div>
+   );
 };
 
 export default AuthPageWrapper;
--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import AuthPageWrapper from "../../components/AuthPageWrapper";
 import Navbar from "../../components/Navbar";
+import { icons } from "../../utils/helpers";
 
 const Login: NextPage = () => {
    return (
@@ -11,6 +13,13 @@ const Login: NextPage = () => {
             <link rel="icon" href="/favicon.ico" />
          </Head>
          <Navbar></Navbar>
+         <AuthPageWrapper
+            icon={icons.authPage.login}
+            heading="Log in to your account"
+            subHeading="Welcome back! Please enter your details."
+         >
+            <div>Contenth</div>
+         </AuthPageWrapper>
       </>
    );
 };
