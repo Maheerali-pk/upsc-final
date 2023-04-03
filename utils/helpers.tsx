97e8b4a461a3c5145a3c873756da2dc5fadff750
Maheerali-pk <maheerali121@gmail.com>
2023-04-04 02:02:34 +0500

    Notification popup element created

--- utils/helpers.tsx
+++ utils/helpers.tsx
@@ -194,6 +194,87 @@ export const icons = {
          />
       </svg>
    ),
+   close: (
+      <svg
+         width="20"
+         height="20"
+         viewBox="0 0 20 20"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg"
+      >
+         <path
+            d="M15 5L5 15M5 5L15 15"
+            stroke="black"
+            stroke-width="1.66667"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+         />
+      </svg>
+   ),
+   safety: (
+      <svg
+         width="44"
+         height="44"
+         viewBox="0 0 44 44"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg"
+      >
+         <g filter="url(#filter0_d_1107_38079)">
+            <rect x="2" y="1" width="40" height="40" rx="8" fill="#FFF4ED" />
+            <path
+               d="M21.302 30.6149C21.5234 30.744 21.6341 30.8086 21.7903 30.8421C21.9116 30.8681 22.0884 30.8681 22.2097 30.8421C22.3659 30.8086 22.4766 30.744 22.698 30.6149C24.646 29.4784 30 25.9084 30 21V16.2176C30 15.4181 30 15.0183 29.8692 14.6747C29.7537 14.3711 29.566 14.1003 29.3223 13.8855C29.0465 13.6424 28.6722 13.5021 27.9236 13.2213L22.5618 11.2107C22.3539 11.1327 22.25 11.0937 22.143 11.0783C22.0482 11.0646 21.9518 11.0646 21.857 11.0783C21.75 11.0937 21.6461 11.1327 21.4382 11.2107L16.0764 13.2213C15.3278 13.5021 14.9535 13.6424 14.6777 13.8855C14.434 14.1003 14.2463 14.3711 14.1308 14.6747C14 15.0183 14 15.4181 14 16.2176V21C14 25.9084 19.354 29.4784 21.302 30.6149Z"
+               stroke="#FF692E"
+               stroke-width="2"
+               stroke-linecap="round"
+               stroke-linejoin="round"
+            />
+            <rect
+               x="2.5"
+               y="1.5"
+               width="39"
+               height="39"
+               rx="7.5"
+               stroke="#FFE6D5"
+            />
+         </g>
+         <defs>
+            <filter
+               id="filter0_d_1107_38079"
+               x="0"
+               y="0"
+               width="44"
+               height="44"
+               filterUnits="userSpaceOnUse"
+               color-interpolation-filters="sRGB"
+            >
+               <feFlood flood-opacity="0" result="BackgroundImageFix" />
+               <feColorMatrix
+                  in="SourceAlpha"
+                  type="matrix"
+                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                  result="hardAlpha"
+               />
+               <feOffset dy="1" />
+               <feGaussianBlur stdDeviation="1" />
+               <feColorMatrix
+                  type="matrix"
+                  values="0 0 0 0 0.0627451 0 0 0 0 0.0941176 0 0 0 0 0.156863 0 0 0 0.05 0"
+               />
+               <feBlend
+                  mode="normal"
+                  in2="BackgroundImageFix"
+                  result="effect1_dropShadow_1107_38079"
+               />
+               <feBlend
+                  mode="normal"
+                  in="SourceGraphic"
+                  in2="effect1_dropShadow_1107_38079"
+                  result="shape"
+               />
+            </filter>
+         </defs>
+      </svg>
+   ),
    arrowLeft: (
       <svg
          width="21"
