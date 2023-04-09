a5fc778ea688264bc412bfc09f475f0429026f92
cvikasreddy <vikas.challaram@gmail.com>
2023-04-09 12:24:38 +0530

    get to GET

--- apis/getProfile.ts
+++ apis/getProfile.ts
@@ -2,9 +2,9 @@ import { customFetch } from "../utils/helpers";
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export const getCompanyProfile = () => {
-   return customFetch({ method: "get", path: "company/profile" });
+  return customFetch({ method: "GET", path: "company/profile" });
 };
 
 export const getCandidateProfile = () => {
-   return customFetch({ method: "get", path: "candidate/profile" });
+  return customFetch({ method: "GET", path: "candidate/profile" });
 };
